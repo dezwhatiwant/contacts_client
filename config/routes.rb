63d3365adfa88787bea489bb01b23f7441055652
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-24 21:31:32 -0500

    added sign up function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/' => 'client/contacts#index'
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
   namespace :client do
     get '/contacts' => 'contacts#index'
     get '/contacts/new' => 'contacts#new'
